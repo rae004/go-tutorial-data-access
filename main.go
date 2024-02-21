518a3bbd4cca434359e03802112b4c2138ac7535
Robert Engel <robert.engel@apollidon.com>
2024-02-20 19:17:04 -0500

    Adding methods to add and query db records.

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
+type Alubm struct {
+	ID     int
+	Title  string
+	Artist string
+	Price  float32
+}
+
 var db *sql.DB
 
 func main() {
@@ -33,6 +40,86 @@ func main() {
 	if pingErr != nil {
 		log.Fatal(pingErr)
 	}
-
 	fmt.Println("Connected!")
+
+	// Query Db for albums by Artist.
+	albums, err := albumsByArtist("John Coltrane")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Albums found: %v\n", albums)
+
+	// Query Db for album by ID.
+	alb, err := AlbumByID(3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Album found: %v\n", alb)
+
+	// Add a new album to the database.
+	albID, err := addAlbum(Alubm{
+		Title:  "Best before 1984",
+		Artist: "Crass",
+		Price:  99.99,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("ID of added album: %v\n", albID)
+}
+
+// albumsByArtist queries for albums that have the specified artist name.
+func albumsByArtist(name string) ([]Alubm, error) {
+	rows, err := db.Query("SELECT * FROM album WHERE artist = ?", name)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var ret []Alubm
+	for rows.Next() {
+		var alb Alubm
+		if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+			return nil, err
+		}
+		ret = append(ret, alb)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return ret, nil
+}
+
+// AlbumByID queries for the album with the specified ID.
+func AlbumByID(id int) (Alubm, error) {
+	var alb Alubm
+
+	row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+		if err == sql.ErrNoRows {
+			return alb, fmt.Errorf("album not found: %v", id)
+		}
+		return alb, fmt.Errorf("could not find album %v: %v", id, err)
+
+	}
+
+	return alb, nil
+}
+
+// addAlbum adds the specified album to the database,
+// returning the album ID of the new entry
+func addAlbum(alb Alubm) (int64, error) {
+	result, err := db.Exec("INSERT INTO album (title, artist, price) VALUES (?, ?, ?)", alb.Title, alb.Artist, alb.Price)
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("addAlbum: %v", err)
+	}
+
+	return id, nil
 }
